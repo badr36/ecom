26d5f697e4feeb2680c168d393721d31b55bcc25
badr elyatim <flipbadr7@gmail.com>
2022-06-20 14:29:20 +0100

    t

---
+++ admin/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/login.css">
+    <title>E-SHOP</title>
+</head>
+<body>
+    <div class="container">
+        <div class="photo">
+            <img src="public/images/admin.png" alt="">
+        </div>
+        <div class="login-form">
+            <h2>E-<span>SHOP</span></h2>
+            <p>Sign into your account</p>
+
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="text" name="password" placeholder="Password" required>
+
+            
+
+            <input type="submit" name="submit" value="Login">
+        </div>
+    </div>
+</body>
+</html>
