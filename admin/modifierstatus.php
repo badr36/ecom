9cea7bf6c15cdbe7e7dbee7c77510725ea20d219
badr elyatim <flipbadr7@gmail.com>
2022-06-22 13:37:08 +0100

    commit

--- admin/modifierstatus.php
+++ admin/modifierstatus.php
@@ -15,6 +15,27 @@ if(isset($_POST['submit']))
     {
         header("location: detailcommande.php?id_commande={$_POST['id']}");
     }
+    else if($_POST['submit'] == 'Livrée')
+    {
+        $status = $commande->query("SELECT status FROM commandes WHERE id=?", array($_POST['id']))->fetch()['status'];
+        if(strcmp($status, 'Livrée') !== 0)
+        {
+            $cmds = $commande->get($_POST['id'])->fetchAll();
+            foreach($cmds as $cmd)
+            {
+                $commande->query("UPDATE produits set stock=stock-{$cmd['qty']} WHERE id={$cmd['id_produit']}");
+            }
+            $commande->query("UPDATE commandes set status=? WHERE id=?", array($_POST['submit'], $_POST['id']));
+
+            header("location: commandes.php");
+        }
+
+        else
+            header("location: commandes.php");
+       
+        
+    }
+
     else
     {
         $commande->query("UPDATE commandes set status=? WHERE id=?", array($_POST['submit'], $_POST['id']));
